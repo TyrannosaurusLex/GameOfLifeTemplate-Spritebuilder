5d784769fbe71a365432726a83c5c1762b08d536
TyrannosaurusLex <lex@Lexs-MacBook-Air.local>
2014-07-08 11:51:05 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -135,7 +135,7 @@ static const int GRID_COLUMNS = 10;
     for (int i = 0;i < [_gridArray count]; i++){
         for (int j =0; j < [_gridArray[i]count]; j++){
             Creature *currentCreature = _gridArray[i][j];
-            
+            printf("currentCreature.livingNeighbors@ [%d][%d] == [%d]",i,j,currentCreature.livingNeighbors);
             if ( currentCreature.livingNeighbors == 3){
                 currentCreature.isAlive = true;
                 printf("currentCreature.isAlive set to true");
