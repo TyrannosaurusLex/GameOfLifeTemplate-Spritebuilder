b36250dc0f85bcedf85032287b98cc8dfe1ddaaf
TyrannosaurusLex <lex@Lexs-MacBook-Air.local>
2014-07-08 09:03:03 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -137,7 +137,7 @@ static const int GRID_COLUMNS = 10;
             
             if ( currentCreature.livingNeighbors == 3){
                 currentCreature.isAlive = true;
-                    printf("currentCreature.isAlive set to true")
+                printf("currentCreature.isAlive set to true");
             }
             else if ( currentCreature.livingNeighbors >= 1 || currentCreature.livingNeighbors <= 4){
                 currentCreature.isAlive = false;
