1c2d3074b275373738bad7bca953072f98b290b6
TyrannosaurusLex <lex@Lexs-MacBook-Air.local>
2014-07-09 09:57:59 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -5,13 +5,13 @@
 //  Created by Lex Lehr on 7/7/14.
 //  Copyright (c) 2014 Apportable. All rights reserved.
 //
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+//#define ANSI_COLOR_RED     "\x1b[31m"
+//#define ANSI_COLOR_GREEN   "\x1b[32m"
+//#define ANSI_COLOR_YELLOW  "\x1b[33m"
+//#define ANSI_COLOR_BLUE    "\x1b[34m"
+//#define ANSI_COLOR_MAGENTA "\x1b[35m"
+//#define ANSI_COLOR_CYAN    "\x1b[36m"
+//#define ANSI_COLOR_RESET   "\x1b[0m"
 
 
 #import "Grid.h"
@@ -170,12 +170,12 @@ static const int GRID_COLUMNS = 10;
     }
     return isIndexValid;
 }
--(void)colorPrintReference{
-    printf(ANSI_COLOR_RED     "This text is RED!"     ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_GREEN   "This text is GREEN!"   ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_YELLOW  "This text is YELLOW!"  ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_BLUE    "This text is BLUE!"    ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_MAGENTA "This text is MAGENTA!" ANSI_COLOR_RESET "\n");
-    printf(ANSI_COLOR_CYAN    "This text is CYAN!"    ANSI_COLOR_RESET "\n");
-}
+//-(void)colorPrintReference{
+//    printf(ANSI_COLOR_RED     "This text is RED!"     ANSI_COLOR_RESET "\n");
+//    printf(ANSI_COLOR_GREEN   "This text is GREEN!"   ANSI_COLOR_RESET "\n");
+//    printf(ANSI_COLOR_YELLOW  "This text is YELLOW!"  ANSI_COLOR_RESET "\n");
+//    printf(ANSI_COLOR_BLUE    "This text is BLUE!"    ANSI_COLOR_RESET "\n");
+//    printf(ANSI_COLOR_MAGENTA "This text is MAGENTA!" ANSI_COLOR_RESET "\n");
+//    printf(ANSI_COLOR_CYAN    "This text is CYAN!"    ANSI_COLOR_RESET "\n");
+//}
 @end
