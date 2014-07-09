56c5a89d0aa86995118e7bd905a9db0b3f6af16c
TyrannosaurusLex <lex@Lexs-MacBook-Air.local>
2014-07-09 12:25:07 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -110,6 +110,7 @@ static const int GRID_COLUMNS = 10;
         // iterate through all the columns for a given row
         for (int j = 0; j < GRID_COLUMNS; j++)
         {
+            printf("countNeighbors innerloop at i: [%d] j: [%d]",i,j);
             // access the creature in the cell that corresponds to the current row/column
             Creature *currentCreature = _gridArray[i][j];
             
