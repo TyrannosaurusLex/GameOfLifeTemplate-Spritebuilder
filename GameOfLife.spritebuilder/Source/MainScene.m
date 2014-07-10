1a24a6518a90407183b151eacef74548e6c921ea
TyrannosaurusLex <lex@Lexs-MacBook-Air.local>
2014-07-10 14:30:02 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/MainScene.m
+++ GameOfLife.spritebuilder/Source/MainScene.m
@@ -25,7 +25,7 @@
         _timer = [[CCTimer alloc] init];
         // XXX - why did I have to add this?  is this correct?
         _grid = [[Grid alloc] init];
-        printf( "MainScene::init - self => [%p]; _grid => [%p]", self, _grid );
+        printf( "MainScene::init - self => [%p]; _grid => [%p]\n", self, _grid );
     }
     
     return self;
@@ -33,7 +33,7 @@
 
 - (void)play
 {
-    printf(" play called - self => [%p] \n", self );
+    printf("MainScene::play called - self => [%p] \n", self );
     //[_grid evolveStep];
     //this tells the game to call a method called 'step' every half second.
     [self schedule:@selector(step) interval:0.5f];
@@ -41,14 +41,14 @@
 
 - (void)pause
 {
-    printf(" pause called - self => [%p]\n", self );
+    printf("MainScene::pause called - self => [%p]\n", self );
     [self unschedule:@selector(step)];
 }
 
 // this method will get called every half second when you hit the play button and will stop getting called when you hit the pause button
 - (void)step
 {
-    printf(" step called - self => [%p]; seqno => [%d]; _grid => [%p]\n", self, seqno, _grid);
+    printf("MainScene::step called - self => [%p]; seqno => [%d]; _grid => [%p]\n", self, seqno, _grid);
     ++seqno;
     [_grid evolveStep];
    // _generationLabel.string = [NSString stringWithFormat:@"%d", _grid.generation];
