6a0f92d63b90b29c998baa5265ae0673eead7fb3
TyrannosaurusLex <lex@Lexs-MacBook-Air.local>
2014-07-11 10:51:48 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/MainScene.m
+++ GameOfLife.spritebuilder/Source/MainScene.m
@@ -25,8 +25,8 @@
     if (self) {
         _timer = [[CCTimer alloc] init];
         // XXX - why did I have to add this?  is this correct?
-        //  _grid = [[Grid alloc] init];
-        //_grid = [Grid alloc];
+        _grid = [[Grid alloc] init];
+        _grid = [Grid alloc];
     }
     printf( "MainScene::init - self => [%p]; _grid => [%p]\n", self, _grid );
     
