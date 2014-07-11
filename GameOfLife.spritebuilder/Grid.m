8ad6dd78479953aa52dd9b8bd6b33297595bcfa0
TyrannosaurusLex <lex@Lexs-MacBook-Air.local>
2014-07-11 11:01:52 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -15,6 +15,7 @@
 
 #import "Grid.h"
 #import "Creature.h"
+#import "MainScene.h"
 
 // these are variables that cannot be changed
 static const int GRID_ROWS = 8;
@@ -29,8 +30,8 @@ static const int GRID_COLUMNS = 10;
     printf( "Grid::init - self => [%p]; &_gridArray => [%p] [%p]\n", self, &_gridArray, _gridArray );
     self = [super init];
     printf( "Grid::init - self => [%p]\n", self );
-    MainScene* parent = self.parent;
-    printf( "Grid::init - self => [%p]; parent => [%p]\n", self, parent );
+    MainScene* parent = [self parent];
+    printf( "Grid::init - self => [%p]; parent => [%p]\n", parent );
     parent.grid = self;
     return self;
 }
